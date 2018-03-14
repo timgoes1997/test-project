bf85b2404d18ca31c23a8e0c0b86b36fe56a6e92
Edwin <edwinvrooij@gmail.com>
2018-03-14 15:07:28 +0100

    update to print more

--- src/main/com/github/fontys/App.java
+++ src/main/com/github/fontys/App.java
@@ -7,6 +7,10 @@ package com.github.fontys;
 public class App {
     public static void main(String args[]) {
         System.out.println("Test print");
+        System.out.println("Test print");
+        System.out.println("Test print");
+        System.out.println("Test print");
+        System.out.println("Test print");
     }
 
     /*
