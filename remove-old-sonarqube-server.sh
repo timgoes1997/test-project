c8fe294272dff7c4b20e9bc9bf69d6c2d0e47555
Edwin <edwinvrooij@gmail.com>
2018-03-21 14:17:41 +0100

    update rm

--- remove-old-sonarqube-server.sh
+++ remove-old-sonarqube-server.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 hash="$(docker container ps --all | grep sonarqube | awk '{print $1;}')"
+echo $hash
 
 
-if [ -z ${hash+x} ]; then docker rm $(docker stop $(docker ps -a -q --filter ancestor=sonarqube --format="{{.ID}}")); else echo "No container running, not removing any sonarqube containers."; fi
+if [ ${hash} ]; then docker rm $(docker stop $(docker ps -a -q --filter ancestor=sonarqube --format="{{.ID}}")); else echo "No container running, not removing any sonarqube containers."; fi
 
 
