86204ce7179af253c46abf7b673fcd7ea3850a0b
Edwin <edwinvrooij@gmail.com>
2018-03-21 13:51:15 +0100

    robust rm old

--- remove-old-sonarqube-server.sh
+++ remove-old-sonarqube-server.sh
@@ -2,5 +2,5 @@
 
 hash="$(docker container ps --all | grep sonarqube | awk '{print $1;}')"
 
-docker container stop "$hash"
-docker container rm "$hash"
+
+if [ -z ${hash+x} ]; then docker container stop "$hash"; else docker container rm "$hash"; fi
