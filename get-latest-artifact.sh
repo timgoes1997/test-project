1001bf24bf383ba179570bde841460ec393b26f9
Edwin van Rooij <edwinvrooij@gmail.com>
2018-04-04 12:31:30 +0200

    update artifactory bash script

--- get-latest-artifact.sh
+++ get-latest-artifact.sh
@@ -1,16 +1,19 @@
-# Artifactory location
-server=http://localhost:8081/artifactory
-repo=snapshot
+#!/bin/bash
+GRADLE_PROPERTIES_FILE=gradle.properties
+ 
+function getProperty {
+    PROP_KEY=$1
+    PROP_VALUE=`cat $GRADLE_PROPERTIES_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+    echo $PROP_VALUE
+}
+CURRENT_VERSION=$(getProperty "currentVersion")
+#CURRENT_VERSION=$(echo $CURRENT_VERSION | sed 's/ *-.*//')
+#echo $CURRENT_VERSION
 
-# Maven artifact location
-name=artifact
-artifact=com/company/$name
-path=$server/$repo/$artifact
-version=`curl -s $path/maven-metadata.xml | grep latest | sed "s/.*<latest>\([^<]*\)<\/latest>.*/\1/"`
-build=`curl -s $path/$version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-jar=$name-$build.jar
-url=$path/$version/$jar
+#curl -u admin:password -Q PUT "http://localhost:8081/artifactory/libs-snapshot-local/test-project/$CURRENT_VERSION/test-project-$CURRENT_VERSON.jar"
 
-# Download
-echo $url
-wget -q -N $url
+# http://localhost:8081/artifactory/libs-snapshot-local/test-project/1.0.8-SNAPSHOT/test-project-1.0.8-SNAPSHOT.jar
+URL="http://localhost:8081/artifactory/libs-snapshot-local/test-project/$CURRENT_VERSION/test-project-$CURRENT_VERSION.jar"
+echo $URL
+
+curl -u admin:password "http://localhost:8081/artifactory/libs-snapshot-local/test-project/$CURRENT_VERSION/test-project-$CURRENT_VERSION.jar" --output build/libs/test-project-$CURRENT_VERSION.jar
