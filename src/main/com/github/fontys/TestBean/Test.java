f11b835f4e96205d6312e16cbaf483a25e7a94ed
tim goes <tim_goes4@hotmail.nl>
2018-04-11 11:18:13 +0200

    Updated bade code

--- src/main/com/github/fontys/TestBean/Test.java
+++ src/main/com/github/fontys/TestBean/Test.java
@@ -13,8 +13,44 @@ public class Test {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response sayHello(){
+    public Response sayHello() {
         return Response.ok("Hello").build();
+    }
+
+    @GET
+    @Path("badcode")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response sayBadcode() {
+
+        int pos1 = 12;
+        int pos2 = 13;
+        int pos3 = 15;
+        int totalNumber = 0;
+
+        for (int x = 0; x < pos1; x++) {
+            totalNumber++;
+            for (int y = 0; y < pos2; y++) {
+                totalNumber++;
+                for(int z = 0; z< pos3; z++){
+                    if(totalNumber > 30){
+                        if(totalNumber < 40) {
+                            switch (totalNumber){
+                                case 31:
+                                    totalNumber += 2;
+                                case 35:
+                                    totalNumber += 5;
+                                default:
+                                    totalNumber *= totalNumber;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        String s = String.format("Bad code %s", totalNumber);
+
+        return Response.ok(s).build();
     }
 
 }
